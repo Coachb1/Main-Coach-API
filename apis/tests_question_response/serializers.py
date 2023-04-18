941ad193165494b8915fa3278d36d8de839d6999
1dsoni <diwanshusoni96@gmail.com>
2023-04-18 12:26:49 +0530

    qid

--- apis/tests_question_response/serializers.py
+++ apis/tests_question_response/serializers.py
@@ -6,11 +6,12 @@ from tests.models import TestQuestionResponse, TestQuestion
 
 class TestQuestionResponseSerializer(serializers.ModelSerializer):
     question = serializers.SerializerMethodField(method_name="get_question", read_only=True)
+    question_id = serializers.CharField(write_only=True)
 
     class Meta:
         model = TestQuestionResponse
-        fields = ["uid", "test_attempt_session_id", "question", "response_file", "response_text", "feedback_text",
-                  "evaluation_status", "created", "updated"]
+        fields = ["uid", "test_attempt_session_id", "question_id", "question", "response_file", "response_text",
+                  "feedback_text", "evaluation_status", "created", "updated"]
 
     def get_question(self, instance):
         return TestQuestionDisplaySerializer(instance=TestQuestion.objects.filter(uid=instance.question_id).last(), many=False).data
