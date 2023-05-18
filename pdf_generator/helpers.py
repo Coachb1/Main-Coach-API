38d9ebb8c44f6d77b3d2e2d4b1c43444d5f99900
Samarth Pawar <samarthpawar@Samarths-MacBook-Pro.local>
2023-05-18 17:03:39 +0530

    report-generator initial commit

--- pdf_generator/helpers.py
+++ pdf_generator/helpers.py
@@ -9,7 +9,7 @@ from documents.choices import DocOwnerTypeChoice, DocTypeChoice
 from documents.helpers import create_document, get_document_url_from_doc_id
 from tenants.helpers import tenant_from_tenant_id
 from tests.db_helpers import get_test_questions_from_test
-from tests.models import Test, TestQuestion
+from tests.models import Test, TestQuestion, TestAttemptSession
 
 options = {
     'page-size': 'Letter',
@@ -45,7 +45,8 @@ def get_flash_cards_from_test(test: Test):
                                                             "text": question.key_learning_point}
         )
 
-        flash_cards.append((question.uid, convert_html_to_pdf(flash_card_html, css_file)))
+        flash_cards.append(
+            (question.uid, convert_html_to_pdf(flash_card_html, css_file)))
 
     saved_flash_cards = []
     for flash_card in flash_cards:
@@ -84,3 +85,74 @@ def get_flash_cards_from_test(test: Test):
         )
 
     return flash_card_urls
+
+
+def get_report_from_test_attempt_session(test_attempt_session: TestAttemptSession):
+    if test_attempt_session.report_doc_id:
+        return get_document_url_from_doc_id(test_attempt_session.report_doc_id)
+
+    tenant = tenant_from_tenant_id(test_attempt_session.tenant_id)
+    test_id = test_attempt_session.test_id
+    test = Test.objects.get(uid=test_id)
+    participant_id = test_attempt_session.participant_id
+    participant_name = User.objects.get(uid=participant_id).name
+    test_started_at = test_attempt_session.started_at
+
+    questions = TestQuestion.objects.filter(test_id=test_id)
+    participant_responses = TestQuestionResponse.objects.filter(
+        test_attempt_session_id=test_attempt_session.uid)
+
+    qa = []
+
+    for question in questions:
+        question_id = question.uid
+        question_text = question.question
+
+        participant_response = None
+
+        # get the response of the participant
+        for response in participant_responses:
+            if response.question_id == question_id:
+                participant_response = response
+                break
+        if participant_response is None:
+            continue
+
+        response_text = participant_response.response_text
+        feedback_text = participant_response.feedback_text
+
+        qa.append({
+            "question_text": question_text,
+            "response_text": response_text,
+            "feedback_text": feedback_text
+        })
+
+        t = render_to_string(
+            f"pdf_generator/reports/report.html", {'qa': qa, 'participant_name': participant_name, 'test_started_at': test_started_at})
+
+        css = os.path.join(settings.BASE_DIR, 'pdf_generator',
+                           'reports', 'static', 'styles_report.css')
+
+        pdf = convert_html_to_pdf(t, css)
+
+        with tempfile.NamedTemporaryFile() as pdf_file:
+            pdf_file.write(pdf)
+            pdf_file.content_type = "application/pdf"
+            pdf_file.size = len(pdf)
+
+            doc = create_document(
+                tenant=tenant,
+                owner_type=DocOwnerTypeChoice.system,
+                owner_id=tenant.uid,
+                display_name=f"report_{test_attempt_session.uid}.pdf",
+                doc_type=DocTypeChoice.REPORT,
+                file=pdf_file
+            )
+
+        TestAttemptSession.objects.filter(
+            uid=test_attempt_session.uid
+        ).update(
+            report_doc_id=doc.uid
+        )
+
+        return get_document_url_from_doc_id(doc.uid)
