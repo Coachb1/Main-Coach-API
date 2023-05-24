2224d14dec376d5d97ce9a10e8c04731dae854af
1dsoni <diwanshusoni96@gmail.com>
2023-05-24 20:30:24 +0530

    added user attribute feature

--- apis/accounts/serializers.py
+++ apis/accounts/serializers.py
@@ -4,10 +4,16 @@ from users.choices import UserRoleChoice
 from users.models import User
 
 
+class UserAttributesUserContextSerializer(serializers.Serializer):
+    tag = serializers.CharField()
+    attributes = serializers.JSONField()
+
+
 class SetupAccountUserContextSerializer(serializers.Serializer):
     name = serializers.CharField()
     role = serializers.ChoiceField(choices=UserRoleChoice)
     password = serializers.CharField(required=False, default=None)
+    user_attributes = UserAttributesUserContextSerializer(required=False)
 
 
 class SetupAccountIdentityContextSerializer(serializers.Serializer):
