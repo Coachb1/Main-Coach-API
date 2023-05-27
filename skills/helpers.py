070135a693398cb6e95914956b6cd9c0601609d3
Samarth Pawar <samarthpawar@Samarths-MacBook-Pro.local>
2023-05-27 08:49:05 +0530

    Resolved Anthropic JSON Decode bug, Added Top X Labels for Leaderboard report and adjusted CSS for report table to fill the page width

--- skills/helpers.py
+++ skills/helpers.py
@@ -14,26 +14,51 @@ def evaluate_response(question_text, response_text, skills):
     "Required from anthropic:" Rate this answer as "very good", "good", "average", "bad", "very bad" in terms for each skill in the list in JSON: "{skills}" Reply "very good", "good", "average", "bad", "very bad" for each skill from the list in a JSON format
     '''
 
-    response = anthropic_completion(prompt, len(skills) * 20)
+    is_evaluated = True
 
-    return json.loads(response)
+    try:
+        response = anthropic_completion(prompt, len(skills) * 50)
+        response = json.loads(response)
+    except json.decoder.JSONDecodeError:
+        is_evaluated = False
+
+    return response, is_evaluated
 
 
 def top_N_leadership_board(skills, N, tenant_id):
-    top_participants = SkillsRating.objects.filter(
+    # Get all skills_rating objects of this tenant
+    skill_rating_objects = SkillsRating.objects.filter(
         deleted=0,
-        skills_info__has_any_keys=skills,
         tenant_id=tenant_id
-    ).order_by(
-        # sum of average scores for each skill in skills list
-        *[f'-skills_info__{skill}__average_score' for skill in skills]
-    )[:N]
+    )
 
-    # get participants name 
-    for participant in top_participants:
-        participant.name = get_user_display_name(User.objects.get(uid=participant.participant_id))
+    participants = []
 
-    return top_participants
+    for obj in skill_rating_objects:
+        
+        skills_info = obj.skills_info
+        average_score = 0
+        skills_dict = {}
+
+        for skill in skills:
+            if skill in skills_info:
+                average_score += skills_info[skill]['average_score']
+                skills_dict[skill] = skills_info[skill]
+
+
+        participants.append({
+            "participant_id": obj.participant_id,
+            "name": get_user_display_name(User.objects.get(uid=obj.participant_id)),
+            "total_questions_attempted": obj.total_questions_attempted,
+            "total_tests_attempted": obj.total_tests_attempted,
+            "average_score": average_score,
+            "skills_info": skills_dict
+        })
+
+    # sort participants based on the sum of average_score of skills in skills list from the skills_info dictionary in skills_rating object
+    participants = sorted(participants, key=lambda x: x['average_score'], reverse=True)
+
+    return participants[:N]
 
 
 def get_participant_info(participant: User):
