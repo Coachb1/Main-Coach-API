6c26079767cb276205c4e68c90c948d7fa34104c
1dsoni <diwanshusoni96@gmail.com>
2023-05-30 19:42:50 +0530

    added user auth

--- tenants/middlewares.py
+++ tenants/middlewares.py
@@ -1,11 +1,16 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 
-from tenants.helpers import tenant_from_client
-from tenants.helpers import tenant_from_request
+from tenants.helpers import tenant_from_client, tenant_from_user
 
 
 class TenantIdentifierMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        request.tenant = SimpleLazyObject(lambda: tenant_from_request(request) or tenant_from_client(request.client))
+        request.tenant = SimpleLazyObject(
+            lambda: get_tenant(request)
+        )
+
+
+def get_tenant(request):
+    return tenant_from_client(request.client) or tenant_from_user(request.auth_user)
