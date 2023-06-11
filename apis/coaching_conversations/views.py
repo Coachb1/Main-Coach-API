29bf0d295079888b0b6bb931603cabc6a7239228
Samarth Singh Pawar <62646546+SamSaver@users.noreply.github.com>
2023-06-11 08:57:42 +0530

    Frontend api (#14)
    * frontend auth and data apis and test code have a Q-prefix and coaching conversation api
    ---------
    
    Co-authored-by: Samarth Pawar <samarthpawar@Samarths-MacBook-Pro.local>

--- apis/coaching_conversations/views.py
+++ apis/coaching_conversations/views.py
@@ -6,17 +6,18 @@ from rest_framework.filters import OrderingFilter
 
 from apis.coaching_conversations.filtersets import CoachingConversationFilterSet
 from apis.coaching_conversations.serializers import CoachingConversationDisplaySerializer, \
-    InitializeCoachingConversationSerializer, ReplyCoachingConversationSerializer
+    InitializeCoachingConversationSerializer, ReplyCoachingConversationSerializer, CoachingConversationReportDataSerializer
 from clients.permissions import IsAuthenticatedClient
 from coaching_conversations.helpers import initialize_coaching_conversation, continue_coaching_conversation
 from coaching_conversations.models import CoachingConversation
 from commons.viewset import ApiViewSet
+from users.permissions import IsAuthenticatedUser
 
 
 class CoachingConversationViewSet(ApiViewSet,
                                   mixins.ListModelMixin):
     queryset = CoachingConversation.objects.filter(deleted=0)
-    permission_classes = (IsAuthenticatedClient,)
+    permission_classes = (IsAuthenticatedClient, IsAuthenticatedUser)
     serializer_class = CoachingConversationDisplaySerializer
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = CoachingConversationFilterSet
@@ -28,7 +29,8 @@ class CoachingConversationViewSet(ApiViewSet,
 
     @action(methods=["POST"], detail=False, url_path="initialize")
     def initialize_coaching_conversation_view(self, request, *args, **kwargs):
-        serializer = InitializeCoachingConversationSerializer(data=request.data)
+        serializer = InitializeCoachingConversationSerializer(
+            data=request.data)
         serializer.is_valid(raise_exception=True)
 
         test_attempt_session_id = serializer.validated_data["test_attempt_session_id"]
@@ -39,7 +41,8 @@ class CoachingConversationViewSet(ApiViewSet,
         )
 
         return Response(
-            data=CoachingConversationDisplaySerializer(instance=next_conversation).data,
+            data=CoachingConversationDisplaySerializer(
+                instance=next_conversation).data,
             status=status.HTTP_201_CREATED
         )
 
@@ -48,8 +51,10 @@ class CoachingConversationViewSet(ApiViewSet,
         serializer = ReplyCoachingConversationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        participant_message_text = serializer.validated_data.get("participant_message_text")
-        participant_message_url = serializer.validated_data.get("participant_message_url")
+        participant_message_text = serializer.validated_data.get(
+            "participant_message_text")
+        participant_message_url = serializer.validated_data.get(
+            "participant_message_url")
 
         next_conversation = continue_coaching_conversation(
             tenant=request.tenant,
@@ -59,6 +64,7 @@ class CoachingConversationViewSet(ApiViewSet,
         )
 
         return Response(
-            data=CoachingConversationDisplaySerializer(instance=next_conversation).data,
+            data=CoachingConversationDisplaySerializer(
+                instance=next_conversation).data,
             status=status.HTTP_201_CREATED
         )
