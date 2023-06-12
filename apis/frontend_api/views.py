9262f497b6a2768e02179d2d3fc990bcc1369c6a
Samarth Pawar <samarthpawar@Samarths-MacBook-Pro.local>
2023-06-12 15:04:46 +0530

    Added URL shortening service

--- apis/frontend_api/views.py
+++ apis/frontend_api/views.py
@@ -6,25 +6,109 @@ from rest_framework.response import Response
 from commons.viewset import ApiViewSet
 from tenants.helpers import tenant_from_subdomain_prefix
 from .serializers import FrontendAuthSerializer, FrontendAccessTokenSerializer
+from .serializers import FrontendLeaderboardReportSerializer
+from .serializers import FrontendCandidateReportSerializer
+from .serializers import FrontendInteractionReportSerializer
+from .serializers import FrontendInteractionSessionReportSerializer
+from .serializers import FrontendCoachingSessionReportSerializer
 from web_auth.helpers import create_new_tokens, get_new_access_token
 from settings import FRONTEND_BASE_URL
+from .report_types import ReportType
+from url_shortener.helpers import check_url_exists, url_shortify
+from url_shortener.models import UrlShortenerMap
+
+import hashlib
 
 
 class FrontendAuthViewSet(ApiViewSet):
 
-    @action(methods=["GET"], detail=False, url_path="get-report-url")
+    @action(methods=["POST"], detail=False, url_path="get-report-url")
     def get_report_url(self, request, *args, **kwargs):
         serializer = FrontendAuthSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         user_id = serializer.validated_data["user_id"]
         report_type = serializer.validated_data["report_type"]
+        shortify_url = serializer.validated_data["shortify_url"]
 
         tokens = create_new_tokens('user-report', 'uid', user_id)
 
         refresh_token = tokens["refresh"]
 
-        url = f"{FRONTEND_BASE_URL}/{report_type}/{refresh_token}"
+        url = f"{FRONTEND_BASE_URL}/{report_type}/{refresh_token}/"
+
+        if report_type == ReportType.LEADERBOARD_REPORT:
+            leaderboard_serializer = FrontendLeaderboardReportSerializer(
+                data=request.data)
+
+            leaderboard_serializer.is_valid(raise_exception=True)
+
+            skills = leaderboard_serializer.validated_data["skills"]
+
+            url = f"{url}?skills={','.join(skills)}"
+
+        elif report_type == ReportType.CANDIDATE_REPORT:
+            candidate_serializer = FrontendCandidateReportSerializer(
+                data=request.data)
+
+            candidate_serializer.is_valid(raise_exception=True)
+
+            candidate_id = candidate_serializer.validated_data["candidate_id"]
+
+            url = f"{url}?candidate_id={candidate_id}"
+
+        elif report_type == ReportType.INTERACTION_REPORT:
+            interaction_serializer = FrontendInteractionReportSerializer(
+                data=request.data)
+
+            interaction_serializer.is_valid(raise_exception=True)
+
+            interaction_id = interaction_serializer.validated_data["interaction_id"]
+
+            url = f"{url}?interaction_id={interaction_id}"
+
+        elif report_type == ReportType.INTERACTION_SESSION_REPORT:
+            session_serializer = FrontendInteractionSessionReportSerializer(
+                data=request.data)
+
+            session_serializer.is_valid(raise_exception=True)
+
+            session_id = session_serializer.validated_data["session_id"]
+
+            url = f"{url}?session_id={session_id}"
+
+        elif report_type == ReportType.COACHING_SESSION_REPORT:
+            session_serializer = FrontendCoachingSessionReportSerializer(
+                data=request.data)
+
+            session_serializer.is_valid(raise_exception=True)
+
+            session_id = session_serializer.validated_data["session_id"]
+
+            url = f"{url}?session_id={session_id}"
+
+        if shortify_url:
+            # compute the hash of the url
+            long_url_hash = hashlib.sha256(url.encode()).hexdigest()
+            # Check if exists in db
+            short_url = check_url_exists(long_url_hash, user_id)
+
+            if short_url:
+                url = short_url
+                print('--'*100)
+                print('Already that url exists in db')
+            else:
+                # Shortify the url
+                long_url = url
+                url = url_shortify(url)
+
+                # save the short url in db
+                UrlShortenerMap.objects.create(
+                    long_url_hash=long_url_hash,
+                    long_url=long_url,
+                    short_url=url,
+                    tenant_id=user_id
+                )
 
         data = {
             "url": url,
