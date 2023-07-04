94b1a5bc268c19853aca1c79921b4fd83768edff
Samarth Pawar <samarthpawar@Samarths-MacBook-Pro.local>
2023-07-04 17:03:44 +0530

    Added logs

--- tests/helpers.py
+++ tests/helpers.py
@@ -931,6 +931,13 @@ def send_report_link_to_email(test: Test, test_attempt_session: TestAttemptSessi
 
 
 def send_report_link_to_whatsapp(test: Test, test_attempt_session: TestAttemptSession, report_url: str):
+
+    logger.info("sending whatsapp message to participant %s with phone %s",
+                test_attempt_session.participant_id, participant_phone)
+
+    logger.info(
+        f"is Report Sent to Whatsapp Boolean: {test_attempt_session.is_report_sent_to_whatsapp}")
+
     if test_attempt_session.is_report_sent_to_whatsapp:
         return
 
@@ -941,6 +948,9 @@ def send_report_link_to_whatsapp(test: Test, test_attempt_session: TestAttemptSe
         user_id=participant_id).attributes
 
     participant_name = participant_attributes.get("user_name")
+
+    logger.info(
+        f"[Whatsapp Send Message Data] Participant Name: {participant_name}, Participant ID: {participant_id},  Test Name: {test_name}, Test Attempt Session ID: {test_attempt_session.uid}")
 
     # Get report url after removing it from the base url
     report_url = report_url.replace(FRONTEND_BASE_URL, "")
