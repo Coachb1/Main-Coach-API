e304c02082f4719e3ed1d12972f24dcae22bf6cd
Diwanshu Soni <diwanshusoni96@gmail.com>
2023-07-07 19:11:42 +0530

    doc pipeline

--- apis/documents/serializer.py
+++ apis/documents/serializer.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
+from documents.choices import DocActionTypeChoice
 from documents.models import Document
+
+
+class DocumentActionsSerializer(serializers.Serializer):
+    action = serializers.ChoiceField(choices=DocActionTypeChoice)
+    context = serializers.JSONField(allow_null=True)
 
 
 class DocumentCreateSerializer(serializers.Serializer):
@@ -9,6 +15,7 @@ class DocumentCreateSerializer(serializers.Serializer):
     display_name = serializers.CharField()
     doc_type = serializers.CharField()
     file = serializers.FileField()
+    actions_pipeline = DocumentActionsSerializer(required=False, allow_null=True, many=True)
 
 
 class DocumentViewSerializer(serializers.ModelSerializer):
