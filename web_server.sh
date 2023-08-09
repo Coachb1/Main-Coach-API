7b04e19200e50d06e160d81a829366371fed58b5
Rajan <bagoriarajan@gmail.com>
2023-08-09 14:45:20 +0530

    new relic uninstalled

--- web_server.sh
+++ web_server.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ./manage.py migrate
-curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=NRAK-OK6LYI5AERXPXO4XHOQSNY0DUFL NEW_RELIC_ACCOUNT_ID=4088629 /usr/local/bin/newrelic install -n logs-integration
-newrelic-admin run-python gunicorn --bind 0.0.0.0:"${WEB_SERVER_PORT:=8000}"\
+gunicorn --bind 0.0.0.0:"${WEB_SERVER_PORT:=8000}"\
          --timeout 120 \
          --graceful-timeout 30 \
          --workers 5 \
