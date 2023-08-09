eee2d23519daf3f212be39150be29dd37d102f5d
Rajan <bagoriarajan@gmail.com>
2023-08-09 14:16:55 +0530

    new relic installation

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+import newrelic.agent
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
+
+newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), "newrelic.ini"))
+application = newrelic.agent.WSGIApplicationWrapper(application)
