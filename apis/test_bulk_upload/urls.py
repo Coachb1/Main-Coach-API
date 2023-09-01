bbd2bcab687ec2de8be6dc16202864f1b88bee10
Rajan <bagoriarajan@gmail.com>
2023-09-01 09:56:34 +0530

    get filler and powerwords from responses api created

--- apis/test_bulk_upload/urls.py
+++ apis/test_bulk_upload/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('test-create-orchestrated-conversation-slack/', views.test_create_orchestrated_conversation_slack, name='test_create_orchestrated_conversation_slack'),
     path('process-file-web/', views.process_web_file, name='process-file-web'),
     path('process-file-slack/', views.process_slack_file, name='process-file-slack'),
-    path('process-orchestrated-conversation-file-slack/', views.process_orchestrated_conversation_slack_file, name='process-orchestrated-conversation-slack-file')
+    path('process-orchestrated-conversation-file-slack/', views.process_orchestrated_conversation_slack_file, name='process-orchestrated-conversation-slack-file'),
+    path('get-power-filler-words/', views.get_filler_and_powerwords, name='get-power-filler-words')
 ]
