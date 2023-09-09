03565f470bca04d51003d508c7bd9feee91c313c
Rajan <bagoriarajan@gmail.com>
2023-09-09 10:57:57 +0530

    gcp upload and gcp get url added

--- commons/s3_get_url.py
+++ commons/s3_get_url.py
@@ -2,17 +2,30 @@ import boto3
 from botocore.config import Config
 
 from commons.timeit import timeit
+from google.cloud import storage
 
 
 @timeit
-def get_url(region_name, bucket, key):
-    s3 = boto3.client('s3', region_name=region_name, config=Config(signature_version='s3v4'))
+def get_url(region_name, bucket, key,expiration_time):
+    # s3 = boto3.client('s3', region_name=region_name, config=Config(signature_version='s3v4'))
 
-    return s3.generate_presigned_url(
-        ClientMethod='get_object',
-        Params={
-            'Bucket': bucket,
-            'Key': key
-        },
-        ExpiresIn=15*60
+    # return s3.generate_presigned_url(
+    #     ClientMethod='get_object',
+    #     Params={
+    #         'Bucket': bucket,
+    #         'Key': key
+    #     },
+    #     ExpiresIn=15*60
+    # )
+    client = storage.Client.from_service_account_json(r'C:\Users\Hello\gcp\bucketaccess.json')
+    # client = storage.Client()
+    bucket = client.get_bucket(bucket)
+    blob = bucket.blob(key)
+
+    url = blob.generate_signed_url(
+        version='v4',
+        expiration=expiration_time,
+        method='GET'
     )
+
+    return url
