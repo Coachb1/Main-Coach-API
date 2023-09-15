d3947352f0e13147094b45852fd27c609f022082
Rajan08091998 <95527358+Rajan08091998@users.noreply.github.com>
2023-09-15 17:38:35 +0530

    Rajan/gcp upload (#179)
    * removed permission for creating tenant, client and account
    
    * Revert "removed permission for creating tenant, client and account"
    
    * gcp upload and gcp get url added
    
    * code updated
    
    * code updated
    
    * requirements.txt updated
    
    ---------
    
    Co-authored-by: Aadil Ansari <ansariaadil611@gmail.com>

--- commons/s3_get_url.py
+++ commons/s3_get_url.py
@@ -2,17 +2,33 @@ import boto3
 from botocore.config import Config
 
 from commons.timeit import timeit
+from google.cloud import storage
+import os
+from pathlib import Path
 
 
 @timeit
 def get_url(region_name, bucket, key):
-    s3 = boto3.client('s3', region_name=region_name, config=Config(signature_version='s3v4'))
+    # s3 = boto3.client('s3', region_name=region_name, config=Config(signature_version='s3v4'))
 
-    return s3.generate_presigned_url(
-        ClientMethod='get_object',
-        Params={
-            'Bucket': bucket,
-            'Key': key
-        },
-        ExpiresIn=15*60
+    # return s3.generate_presigned_url(
+    #     ClientMethod='get_object',
+    #     Params={
+    #         'Bucket': bucket,
+    #         'Key': key
+    #     },
+    #     ExpiresIn=15*60
+    # )
+    os.chdir(f"{Path(__file__).resolve().parent}")
+    client = storage.Client.from_service_account_json(r'bucketaccess.json')
+    # client = storage.Client()
+    bucket = client.get_bucket(bucket)
+    blob = bucket.blob(key)
+
+    url = blob.generate_signed_url(
+        version='v4',
+        expiration=15*60,
+        method='GET'
     )
+
+    return url
