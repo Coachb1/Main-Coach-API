9495a7d1be9fb87343a1ed728f15295b1611fd7e
Rajan <bagoriarajan@gmail.com>
2023-10-03 17:33:18 +0530

    new user client_admin added

--- users/choices.py
+++ users/choices.py
@@ -4,3 +4,4 @@ from djchoices import DjangoChoices, ChoiceItem
 class UserRoleChoice(DjangoChoices):
     admin = ChoiceItem("admin")
     member = ChoiceItem("member")
+    client_admin = ChoiceItem("client_admin")
