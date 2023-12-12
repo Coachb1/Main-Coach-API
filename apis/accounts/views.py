0a00b6647581ea4c44c3c24ba94d76cdd0ffc039
Rajan <bagoriarajan@gmail.com>
2023-12-12 17:04:28 +0530

    code updated

--- apis/accounts/views.py
+++ apis/accounts/views.py
@@ -253,8 +253,7 @@ class AccountsViewSet(ApiViewSet,
     @action(methods=['GET'],detail=False, url_path="get-my-lib-data")
     def get_my_lib_data(self,request,*args, **kwargs):
         # test_codes = request.query_params.get('test_codes').split(',')
-        group_name = request.query_params.get('group')
-
+        group_name = request.data.get('group',None)
         tests = Test.objects.filter(deleted=0,client_name=group_name)
         data = []
         for item in tests:
