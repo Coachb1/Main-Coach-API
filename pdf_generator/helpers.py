089209ba9dfa2b67d1f1199dfbe377b55a7405d8
aadil611 <ansariaadil611@gmail.com>
2023-12-27 14:14:21 +0530

    fixed syntax error

--- pdf_generator/helpers.py
+++ pdf_generator/helpers.py
@@ -411,7 +411,7 @@ def get_report_from_test_attempt_session(test_attempt_session: TestAttemptSessio
         print(f"focus area : {test_attempt_session.skills_explanation['mcq_skills']}")
         focus_area = test_attempt_session.skills_explanation['mcq_skills'] if test.test_type == TestTypeChoices.dynamic_mcq else ''
         
-        return {'test_type':test.test_type,'competency_data':competency_report_data,"ui_information":test.ui_information,"certificate_details":test.certificate_details,'scenario_case':test.scenario_case,"title":test.title,'candidate_type': test.candidate_type, 'test_description': test.description, 'qa': qa, 'participant_name': participant_name, 'test_started_at': test_started_at, 'custom_rating': custom_rating,"mcq_summary": test_attempt_session.mcq_summary',focus_area': focus_area}
+        return {'test_type':test.test_type,'competency_data':competency_report_data,"ui_information":test.ui_information,"certificate_details":test.certificate_details,'scenario_case':test.scenario_case,"title":test.title,'candidate_type': test.candidate_type, 'test_description': test.description, 'qa': qa, 'participant_name': participant_name, 'test_started_at': test_started_at, 'custom_rating': custom_rating,"mcq_summary": test_attempt_session.mcq_summary,'focus_area': focus_area}
 
 
     qa = []
