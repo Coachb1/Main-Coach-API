293f3204a9d641e718643086920cf20b3bff7fd3
Rajan <bagoriarajan@gmail.com>
2024-01-06 16:12:20 +0530

    mod in api

--- apis/coaching_conversations/views.py
+++ apis/coaching_conversations/views.py
@@ -304,7 +304,9 @@ class CoachingConversationViewSet(ApiViewSet,
             bot_ids = list(set(SignatureBot.objects.filter(deleted=0).values_list('uid',flat=True)))
             sessions = TestAttemptSession.objects.filter(deleted=0,tenant_id=tenant.uid,test_id__in=bot_ids,participant_id=user_id)
             data = []
-            data.append(get_bot_conversation_data_user(sessions,tenant,user_id))
+            data_conv = get_bot_conversation_data_user(sessions,tenant,user_id)
+            if len(data_cov['results']) > 0 :
+                data.append(data_conv)
             return Response(data, status=status.HTTP_200_OK)
         
         else:
