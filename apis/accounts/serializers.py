e146c8109929191a42135d4234d3ba812c08716e
aadil611 <ansariaadil611@gmail.com>
2024-01-13 17:19:36 +0530

    added coachcocheementormentee profile api

--- apis/accounts/serializers.py
+++ apis/accounts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from users.choices import UserRoleChoice
-from users.models import User
+from users.models import User, CoachCoacheeMentorMenteeProfile
+from commons.cloudinary import upload_image
 
 
 class UserAttributesUserContextSerializer(serializers.Serializer):
@@ -30,3 +31,18 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["uid", "name", "role", "created", "updated"]
+
+
+
+class CoachCoacheeMentorMenteeProfileSerializer(serializers.ModelSerializer):
+    profile_image = serializers.FileField(required=False)
+    class Meta:
+        model = CoachCoacheeMentorMenteeProfile
+        fields = ['uid','profile_type','name', 'email', 'status', 'speciality', 'experience', 'location','profile_image',
+                'favourite_simulation_codes', 'about', 'department', 'unique_id', 'user_id', 'bot_ids', 'bot_urls', 'profile_image_url']
+
+    def create(self, validated_data):
+        if validated_data.get('profile_image'):
+            validated_data['profile_image_url'] = upload_image(validated_data['profile_image']).get('secure_url')
+            validated_data.pop('profile_image')
+        return CoachCoacheeMentorMenteeProfile.objects.create(**validated_data)
---
+++ commons/cloudinary.py
@@ -0,0 +1,24 @@
+import cloudinary
+from cloudinary import uploader
+from commons.timeit import timeit
+from logging import getLogger
+
+logger = getLogger(__name__)
+          
+cloudinary.config( 
+  cloud_name = "dtbl4jg02", 
+  api_key = "313873736982566", 
+  api_secret = "uhb8rZ5Tv8pca_VY29pdhR63caI" 
+)
+
+@timeit
+def upload_image(image):
+    """
+    Upload image to cloudinary
+    """
+    try:
+        res = uploader.upload(image)
+        return res
+    except Exception as e:
+        logger.error(f"Error in uploading image to cloudinary {e}")
+        raise e
