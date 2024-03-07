1bae6d4aefb4ebfa0e8b01c3be13e3c103d06479
Rajan <bagoriarajan@gmail.com>
2024-03-07 17:58:07 +0530

    apschedular bug fixes

--- email_sender/apps.py
+++ email_sender/apps.py
@@ -1,8 +1,16 @@
 from django.apps import AppConfig
+import os
 
 class EmailSenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'email_sender'
+
+    def ready(self) -> None:
+        from email_sender.jobs import initialize_weekly_email_schedular
+        print(os.environ.get('RUN_MAIN'))
+        if os.environ.get('RUN_MAIN'):
+            initialize_weekly_email_schedular()
+
 
 
         
