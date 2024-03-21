90b7689ab92c06c589709d69a5b86298b0f40161
Rajan <bagoriarajan@gmail.com>
2024-03-21 10:53:41 +0530

    bug fixed"

--- apis/accounts/views.py
+++ apis/accounts/views.py
@@ -670,14 +670,8 @@ class AccountsViewSet(ApiViewSet,
             logger.info(f"************************************** request files : {request}**************************************************************************** request data: {request.data}")
             data = request.data.copy()
             data['tenant_id'] = self.request.tenant.uid
-            profile_approved = data.get('is_approved',None)
-            if profile_approved:
-                if profile_approved.lower() == 'true':
-                    profile_approved = True
-                elif profile_approved.lower() == 'false':
-                    profile_approved = False
-                else:
-                    profile_approved = False
+            profile_approved = data.get('is_approved',False)
+            
 
             if profile_approved:
                 data['is_approved'] = profile_approved
@@ -861,13 +855,7 @@ class AccountsViewSet(ApiViewSet,
                     return Response({"error": "bot_base_url is required"},status=status.HTTP_400_BAD_REQUEST)
                 
                 bot_approved = data.get('is_approved',False)
-                if bot_approved:
-                    if bot_approved.lower() == 'true':
-                        bot_approved = True
-                    elif bot_approved.lower() == 'false':
-                        bot_approved = False
-                    else:
-                        bot_approved = False
+                
 
                 faqs = data.get('faqs')
                 fitment_details = data.get('fitment_data',None)
