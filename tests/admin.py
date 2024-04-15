21909a68711bca3b88ea69fffdd5aee9259577fa
Rajan <bagoriarajan@gmail.com>
2024-04-15 14:14:31 +0530

    code updated

--- tests/admin.py
+++ tests/admin.py
@@ -6,9 +6,11 @@ from tests.models import Test
 
 
 class TestAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_per_page = 10
     list_display = ('uid','test_code','title','test_type','interaction_mode','deleted')
     search_fields = ('test_code',)
     list_editable = ('deleted',)
+    list_filter = ('tenant_id','test_type')
 
 
 admin.site.register(Test, TestAdmin)
