41b7ce72df126a7315e8f5f00b81493ecb25898c
Rajan <bagoriarajan@gmail.com>
2024-04-29 14:16:38 +0530

    code updated

--- email_sender/apps.py
+++ email_sender/apps.py
@@ -5,9 +5,14 @@ class EmailSenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'email_sender'
 
+    _scheduler = None
+
     def ready(self) -> None:
         from email_sender.jobs import initialize_weekly_email_schedular        
-        initialize_weekly_email_schedular()
+        if not self._scheduler:
+            self._scheduler = initialize_weekly_email_schedular()
+            self._scheduler.start()
+        
 
 
 
