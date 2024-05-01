23a472b859d9fa156e68e1ace7fe0bad6cf26e44
Rajan <bagoriarajan@gmail.com>
2024-05-01 17:54:42 +0530

    code updated

--- apis/tests_attempt_session/views.py
+++ apis/tests_attempt_session/views.py
@@ -719,6 +719,7 @@ class TestAttemptSessionViewSet(ApiViewSet,
         except Exception as e:
             logger.exception(e)
             connected = False
+            coach_profile = None
         # previous_conversations = CoachingConversation.objects.filter(
         # tenant_id=self.request.tenant.uid,
         # test_attempt_session_id=test_attempt_session.uid,
@@ -756,16 +757,24 @@ class TestAttemptSessionViewSet(ApiViewSet,
 
         
 
-        # for email in [submitted_email, bot_owner_email,"info@coachbots.com"]:
+        # for email in [submitted_email, bot_owner_email,"coachbots@googlegroups.com"]:
             # send_bot_conversation_email(candidate_name, conv, recepients)
-        recepients = [submitted_email,"info@coachbots.com"]
+        recepients = [submitted_email,"coachbots@googlegroups.com"]
         if connected:
             recepients.append(bot_owner_email)
-        # recepients = ["ansariaadil611@gmail.com","aadil611ofc@gmail.com","info@coachbots.com"]
+
+        coach_name = ""
+        if coach_profile:
+            coach_name = coach_profile.name
+        else:
+            coach_name = bot_att.bot_name
+
+
+        # recepients = ["ansariaadil611@gmail.com","aadil611ofc@gmail.com","coachbots@googlegroups.com"]
         
         logger.info(f"************** session_qna_data conv: {conv}")
         try:
-            send_bot_conversation_email(candidate_name, conv, recepients, conversation_summary, created_scenarios, signature_bot.bot_id, coach_profile.name,bot_att.bot_name, allow_reply=True)
+            send_bot_conversation_email(candidate_name, conv, recepients, conversation_summary, created_scenarios, signature_bot.bot_id, coach_name,bot_att.bot_name, allow_reply=True)
         except Exception as e:
             logger.error({"!!!!!!!!!!!!!!!ERROR": e},exc_info=True)
             send_error_notification("send_bot_transcript_email",f"Error in sending bot transcript email: {e}",{"participant_id":participant_id,"session_id":test_attempt_session_id,"submitted_email":submitted_email})
@@ -841,7 +850,7 @@ class TestAttemptSessionViewSet(ApiViewSet,
                     "answer": value
                 })
 
-        for email in [bot_owner_email,"info@coachbots.com"]:
+        for email in [bot_owner_email,"coachbots@googlegroups.com"]:
             try:
                 send_feedback_conversation_email(user_email,conv,email,type_of_email,is_positive= is_positive.lower() == 'true')
             except Exception as e:
