9e5820f17026be8ada8ad16d0081ea369eca8cd6
Rajan <bagoriarajan@gmail.com>
2024-06-08 14:42:56 +0530

    prompt updated

--- apis/accounts/views.py
+++ apis/accounts/views.py
@@ -1162,11 +1162,7 @@ class AccountsViewSet(ApiViewSet,
                         updated_fields.append("faqs")
 
                     if bot_type == BotTypeChoice.avatar_bot:
-                        try:
-                            prompt = SignatureBot.objects.filter(tenant_id=self.request.tenant.uid,deleted=0).first().custom_prompt
-                        except Exception as e:
-                            prompt = signature_bot_default_prompt()
-                        signature_bot.custom_prompt = prompt
+                        signature_bot.custom_prompt = signature_bot_default_prompt()
                         updated_fields.append("custom_prompt")
 
                     if bot_type == BotTypeChoice.deep_dive:
