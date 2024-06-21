a905dcacc815673bd0ef34fd982a2367912d58be
Rajan <bagoriarajan@gmail.com>
2024-06-21 18:43:33 +0530

    bugs handling added

--- commons/google_apis.py
+++ commons/google_apis.py
@@ -229,11 +229,7 @@ def gemini_completion(prompt,model="gemini-1.0-pro"):
         "top_p": 1
     }
     safety_settings={
-          generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    }
+              }
 
     model = GenerativeModel(
         model,
