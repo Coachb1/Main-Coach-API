e4334d1c258a3591edb3eff58af2291d37f58cfd
Rajan <95527358+Rajan08091998@users.noreply.github.com>
2024-09-06 23:08:49 +0530

    Rajan/mail box v2 (#325)
    * conversation api updated
    
    * added followp system

--- mail_box/admin.py
+++ mail_box/admin.py
@@ -3,17 +3,22 @@ from mail_box.models import MailBox, AuthorizedEmails, EmailConversation
 
 @admin.register(MailBox)
 class MailBoxAdmin(admin.ModelAdmin):
-    list_display = ('uid', 'email', 'prompt','followup_prompt','document_data' ,'created', 'updated', 'deleted')
-    search_fields = ('email', 'prompt')
+    list_display = ('uid', 'grant_id' ,'email', 'prompt','followup_prompt','followup_prompt2', 'reward_prompt1', 'reward_prompt2','document_data' ,'created', 'updated', 'deleted')
+    search_fields = ('email', 'prompt', 'grant_id')
     list_filter = ('created', 'updated', 'deleted', 'email')
-    list_editable = ('email', 'prompt','followup_prompt', 'deleted')
+    list_editable = ('email', 'prompt','followup_prompt','followup_prompt2', 'reward_prompt1', 'reward_prompt2', 'deleted')
 
 @admin.register(AuthorizedEmails)
 class AuthorizedEmailsAdmin(admin.ModelAdmin):
-    list_display = ('uid', 'email', 'user_id', 'is_black_list', 'is_whitelist', 'created', 'updated', 'deleted')
-    search_fields = ('email', 'user_id')
+    list_display = ('uid', 'mailbox_id', 'email', 'user_id', 'is_black_list', 'is_whitelist', 
+                    'name','age','goal','situation','followup_fequency',
+                  'followup_escalation_email','reward_emails',
+                  'created', 'updated', 'deleted')
+    search_fields = ('email', 'user_id','mailbox_iid')
     list_filter = ('is_black_list','is_whitelist', 'created', 'updated', 'deleted')
-    list_editable = ('email', 'user_id', 'is_black_list','is_whitelist', 'deleted')
+    list_editable = ('email', 'user_id', 'is_black_list','is_whitelist', 
+                     'name','age','goal','situation','followup_fequency',
+                  'followup_escalation_email','reward_emails','deleted')
 
 @admin.register(EmailConversation)
 class EmailConversationAdmin(admin.ModelAdmin):
--- mail_box/models.py
+++ mail_box/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from commons.db.model import MyModel
+from mail_box.choices import FollowupFreqType
 
 # Create your models here.
 class MailBox(MyModel):
@@ -7,6 +8,11 @@ class MailBox(MyModel):
     prompt = models.TextField(null=True, blank=True, default=None)
     followup_prompt = models.TextField(null=True, blank=True, default=None)
     document_data = models.JSONField(null=True, blank=True, default=dict)
+    followup_prompt2 = models.TextField(null=True, blank=True, default=None)
+    reward_prompt1 = models.TextField(null=True, blank=True, default=None)
+    reward_prompt2 = models.TextField(null=True, blank=True, default=None)
+    grant_id = models.CharField(max_length=255,null=True,blank=True,default=None)
+
 
     class Meta:
         db_table = 'mail_box'
@@ -16,11 +22,18 @@ class MailBox(MyModel):
 
 
 class AuthorizedEmails(MyModel):
+    mailbox_id = models.CharField(max_length=255,default=None)
     email = models.CharField(max_length=255)
     user_id = models.CharField(max_length=255, null=True, blank=True, default=None)
     is_black_list = models.BooleanField(null=True, default=False)
     is_whitelist = models.BooleanField(null=True, default=False)
-
+    name = models.CharField(max_length=255, null=True, blank=True, default=None)
+    age = models.CharField(max_length=255, null=True, blank=True, default=None)
+    goal = models.CharField(max_length=255, null=True, blank=True, default=None)
+    situation = models.TextField(null=True, blank=True, default=None)
+    followup_fequency = models.CharField(max_length=255,choices=FollowupFreqType,null=True,blank=True,default=FollowupFreqType.nan)
+    followup_escalation_email = models.CharField(max_length=255,null=True,blank=True,default=None)
+    reward_emails = models.BooleanField(null=True, default=True)
 
     class Meta:
         db_table = 'authorized_emails'
