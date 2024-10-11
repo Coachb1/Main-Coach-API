62dc546061e316d2bd913114948bb1d9ba625028
rajan <bagoriarajan@gmail.com>
2024-10-11 12:55:39 +0530

    added teant_id and mailbox id dropdown

--- tenants/models.py
+++ tenants/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from commons.db.model import MyModel
 from tenants.choices import SubscriptionChoices
+from commons.cache_utils import get_cache, set_cache
 
 
 class Tenant(MyModel):
@@ -17,7 +18,14 @@ class Tenant(MyModel):
     web_test_code_json = models.JSONField(null=True,blank=True,default=None)
     use_skills_from_skill_bank = models.BooleanField(default=False, null=True, blank=False)
 
-
+    @staticmethod
+    def get_tenant_choices():
+        choices = get_cache('tenant_choices')
+        if not choices:
+            choices = [(tenant.uid, f"{tenant.name} ({tenant.subdomain_prefix})") for tenant in Tenant.objects.all()]
+            set_cache('tenant_choices', choices, timeout=3600)  # Cache for 1 hour
+        return choices
+    
     class Meta:
         db_table = 'tenant'
 
