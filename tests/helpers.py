c11bae1e56a733768714d215b314f1753e9cdef0
rajan <bagoriarajan@gmail.com>
2024-11-09 10:36:53 +0530

    feedback bug fix

--- tests/helpers.py
+++ tests/helpers.py
@@ -2198,10 +2198,11 @@ def __process_test_response(question: TestQuestion, test: Test, test_attempt_ses
             }
         }
 
-        feedback_text = re.sub(r'\([^)]*\)', '', feedback_text)   # to remove any word limit in ()
-        test_question_response.feedback_text = feedback_text
-        updated_fields.append("feedback_text")
-        updated_fields.append("metadata")
+        if feedback_text:
+            feedback_text = re.sub(r'\([^)]*\)', '', feedback_text)   # to remove any word limit in ()
+            test_question_response.feedback_text = feedback_text
+            updated_fields.append("feedback_text")
+            updated_fields.append("metadata")
 
 
     if test.scenario_case == ScenarioCaseChoices.pitch:
