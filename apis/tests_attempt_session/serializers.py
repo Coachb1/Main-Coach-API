a89df85c4a5fded972ec2f217136e4f2d991347c
rajan <bagoriarajan@gmail.com>
2024-12-18 13:09:15 +0530

    game type scenario added

--- apis/tests_attempt_session/serializers.py
+++ apis/tests_attempt_session/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from tests.models import TestAttemptSession
+from tests.models import TestAttemptSession, TestQuestionResponse
 
 
 class TestAttemptSessionSerializer(serializers.ModelSerializer):
@@ -11,3 +11,15 @@ class TestAttemptSessionSerializer(serializers.ModelSerializer):
         fields = ["uid", "test_id", "participant_id", "test_invite_id", "expires_at", "started_at", "finished_at",
                     "skills_rating", "current_question_idx", "next_question_idx", "test_score", "created", "updated",
                     "status", "is_signature_bot", "is_idp_discussion_opted"]
+
+    def to_representation(self, instance:TestAttemptSession):
+        data = super().to_representation(instance)
+
+        que_response = TestQuestionResponse.objects.filter(
+                                        test_attempt_session_id=instance.uid,
+                                        deleted=False
+                                    ).last()
+        if que_response:
+            data['next_question_text'] = que_response.question_text
+        return data
+        
