c21c5d5a774085bd39c27353f682692d82a65425
rajan <bagoriarajan@gmail.com>
2024-12-20 13:04:20 +0530

    chat_completion updation

--- tests/helpers.py
+++ tests/helpers.py
@@ -955,7 +955,8 @@ def create_test_question_answer_session(tenant: Tenant,
                                 }],
                                 temperature=0,
                                 top_p=0,
-                                models=['gemini-2.0-flash-exp',"gemini-1.5-flash-001","gemini-1.5-pro-001","gemini-1.0-pro"]
+                                models=['gemini-2.0-flash-exp',"gemini-1.5-flash-001","gemini-1.5-pro-001","gemini-1.0-pro"],
+                                instructions=["**Always Present Full Level Details**: Consistently show all levels and options exactly as scripted. Complete all levels and only give feedback as last. Every level and option must be displayed in full, without omission or partial rendering of any text."]
                             )
 
         TestQuestionResponse.objects.create(
@@ -3079,8 +3080,8 @@ def process_dynamic_game(test_question_response:TestQuestionResponse, test:Test
         previous_conv=previous_conversation,
         temperature=0,
         top_p=0,
-        models=['gemini-2.0-flash-exp',"gemini-1.5-flash-001","gemini-1.5-pro-001","gemini-1.0-pro"]
-
+        models=['gemini-2.0-flash-exp',"gemini-1.5-flash-001","gemini-1.5-pro-001","gemini-1.0-pro"],
+        instructions=["**Always Present Full Level Details**: Consistently show all levels and options exactly as scripted. Complete all levels and only give feedback as last. Every level and option must be displayed in full, without omission or partial rendering of any text."]
     )
 
     print(next_question)
