ccf2db4760a832d089a85d9c07cc346c5baeaac7
Rajan <bagoriarajan@gmail.com>
2025-04-10 13:42:35 +0530

    ask access code added

--- users/models.py
+++ users/models.py
@@ -320,6 +320,7 @@ class ClientUserInfo(TenantAwareModel):
     allow_access_to_snippet = models.BooleanField(default=True)
     report_on = models.BooleanField(null=True,blank=True)
     show_recommendations = models.BooleanField(default=True)
+    ask_access_code = models.BooleanField(default=True)
 
     class Meta:
         db_table = "client_user_info"
