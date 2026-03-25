9f9a5e331fd00c36b8aab56c104f451e4cb9fdfe
Rajan <bagoriarajan@gmail.com>
2026-03-25 13:01:02 +0530

    added aiadopts doc

--- apis/analytics/urls.py
+++ apis/analytics/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from apis.analytics.views.event_views import EventViewSet
 from apis.analytics.views.progress_views import ConceptProgressViewSet
-from apis.analytics.view import EventViewSet as AnalyticsEventViewSet
+from apis.analytics.view import EventViewSet as AnalyticsEventViewSet, ai_adopts_doc
 
 router = SimpleRouter()
 router.register("v1/analytics", AnalyticsEventViewSet, basename="analytics")
@@ -12,4 +12,5 @@ router.register("v1/analytics-progress", ConceptProgressViewSet, basename="analy
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("v1/ai-adopts-doc", ai_adopts_doc, name='aiadopts_docs')
 ]
